74f6734592316d9380e2b3b235e15fbafe58b795
NarinhaOfficial <narinhaofficial@gmail.com>
2024-03-01 13:31:38 -0300

    Ajustar imagem

---
+++ 5 - Atividade/brilho-e-constraste.py
@@ -0,0 +1,56 @@
+'''▪Implemente um programa que realize o ajuste de brilho (teclas 'a' e 'z'), 
+ajuste de contraste (teclas 's' e 'x') e aplique o efeito negativo (tecla 'n')'''
+import cv2
+
+original = cv2.imread('C:/Users/narin/OneDrive/Documentos/Programas/processamento de imagens/1 - Atividade/color_red.jpg')
+modificar = cv2.imread('C:/Users/narin/OneDrive/Documentos/Programas/processamento de imagens/1 - Atividade/color_red.jpg')
+
+brilho = 50
+contraste = 1.5
+
+while True:
+    chave = cv2.waitKey(1)
+    if chave == ord('a'):
+        for altura in range (original.shape[0]):
+            for largura in range (original.shape[1]):
+                for canais in range (original.shape[2]):
+                    modificar[altura, largura, canais] = min(255, modificar[altura, largura, canais]+brilho)
+        #cv2.imshow('a - modificar brilho', modificar)
+    elif chave == ord('z'):
+        for altura in range(modificar.shape[0]):
+            for largura in range(modificar.shape[1]):
+                for canais in range(modificar.shape[2]):
+                    modificar[altura, largura, canais] = max(0, modificar[altura, largura, canais]-brilho)
+        #cv2.imshow('z - Diminuir brilho', modificar)
+    elif chave == ord('s'):
+        for a in range(modificar.shape[0]):
+            for l in range(modificar.shape[1]):
+                for c in range(modificar.shape[2]):
+                    modificar[a, l, c] = min(255, modificar[a,l,c] * contraste)
+    elif chave == ord('x'):
+        for a in range(modificar.shape[0]):
+            for l in range(modificar.shape[1]):
+                for c in range(modificar.shape[2]):
+                    modificar[a, l, c] = max(0, modificar[a,l,c] / contraste)
+
+    elif chave == ord('n'):
+        for a in range(modificar.shape[0]):
+            for l in range(modificar.shape[1]):
+                for c in range(modificar.shape[2]):
+                    modificar[a, l, c] = 255 - modificar[a, l, c]
+    elif chave == ord('m'):
+        for a in range(modificar.shape[0]):
+            for l in range(modificar.shape[1]):
+                for c in range(modificar.shape[2]):
+                    modificar[a, l, c] = original [a,l,c]
+         
+    cv2.imshow('a - Aumentar o brilho | z - Diminuir brilho | s - Aumentar o contraste | x - Diminuir o contraste | n - Aplicar negativo | m - Reverter todas as alterações', modificar)
+    cv2.imshow('Original', original)
+    
+    
+    if chave == ord('q'):
+            break
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
