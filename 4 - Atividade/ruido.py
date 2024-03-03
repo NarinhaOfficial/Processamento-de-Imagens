ffb299209174d892bcac68b7535d79c4f6de73d4
NarinhaOfficial <narinhaofficial@gmail.com>
2024-03-03 19:03:28 -0300

    Sal e pimenta

---
+++ 4 - Atividade/ruido.py
@@ -0,0 +1,48 @@
+import random
+import cv2
+import numpy as np
+
+original = cv2.imread('C:/Users/narin/OneDrive/Documentos/Programas/processamento de imagens/1 - Atividade/color_red.jpg')
+
+valor = 0.01
+
+while True:
+    chave = cv2.waitKey(0)
+    modificada = np.zeros(original.shape, np.uint8)
+    copia = modificada.copy()
+    barulho = 1 - valor
+  
+    if chave == ord('a'):
+        if valor < -0.01:
+            valor = 0.01
+        for a in range(original.shape[0]):
+            for l in range(original.shape[1]):
+                sorteio = random.random()
+                if sorteio<valor:
+                    modificada[a][l] = 0
+                elif sorteio > barulho:
+                    modificada[a][l] = 255
+                else:
+                    modificada[a][l] = original[a][l]
+        valor+=0.01
+ 
+    elif chave == ord('s'):
+        if valor < -0.01:
+            valor = 0.01
+        for a in range(original.shape[0]):
+            for l in range(original.shape[1]):
+                sorteio = random.random()
+                if sorteio<valor:
+                    modificada[a][l] = 0
+                elif sorteio > barulho:
+                    modificada[a][l] = 255
+                else:
+                    modificada[a][l] = original[a][l]
+        valor -=0.01
+
+    cv2.imshow('Original', original)
+    cv2.imshow('Sal e Pimenta', modificada)
+    if chave == ord('q'):
+        break
+cv2.waitKey(0)
+cv2.destroyAllWindows()
